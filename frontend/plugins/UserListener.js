a75867a36f753831f20ed2075d89d4cabfa1f6e7
aaron <aaron@mindvalley.com>
2020-03-30 01:33:34 +0800

    additional logged in user handling

--- frontend/plugins/UserListener.js
+++ frontend/plugins/UserListener.js
@@ -2,12 +2,15 @@
 // https://firebase.google.com/docs/auth/web/manage-users?authuser=1
 
 export default ({ app }, inject) => {
+    app.$storage.syncUniversal("accessToken")
+
     app.$fireAuth.onAuthStateChanged(function (user) {
         if (user) {
             user = JSON.parse(JSON.stringify(user))
             app.$storage.setState("user", user)
             app.$storage.setLocalStorage("user", user)
             app.$storage.setState("loggedIn", true)
+            app.$storage.setLocalStorage("loggedIn", true)
 
             // Store accessToken seperately for convenience
             // Also synchronises it to Vuex/Local Storage/Cookies
@@ -18,6 +21,7 @@ export default ({ app }, inject) => {
         } else {
             // No user is signed in.
             app.$storage.setState("loggedIn", false)
+            app.$storage.setLocalStorage("loggedIn", false)
         }
     });
 
