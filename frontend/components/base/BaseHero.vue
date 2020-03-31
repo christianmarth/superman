2d79eab4eed44fe30b79f59d54c03676233abe1b
aaron <aaron@mindvalley.com>
2020-04-01 00:47:01 +0800

    basic hero component

---
+++ frontend/components/base/BaseHero.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="relative bg-gray-50 overflow-hidden">
+    <div class="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
+      <div class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
+        <div class="text-center">
+          <h2
+            class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl"
+          >
+            Halal,
+            <br class="xl:hidden" />
+            <span class="text-indigo-600">it's a lifestyle.</span>
+          </h2>
+          <p
+            class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+          >Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+          <div class="mt-8 mx-auto max-w-3xl">
+            <div class="flex rounded-md shadow-sm">
+              <span
+                class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-4xl"
+              >Search</span>
+              <input
+                id="search"
+                class="flex-1 form-input block w-full rounded-none rounded-r-md transition duration-150 ease-in-out sm:leading-5"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <header class="bg-white shadow-sm">
-      <div class="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-        <h2 class="text-lg leading-6 font-semibold text-gray-900">Dashboard</h2>
-      </div>
-    </header>
+    <BaseHero />
     <main>
       <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
         <!-- Replace with your content -->
@@ -24,8 +20,12 @@
 
 <script>
 import query from "~/services/SelectAllMerchants/SelectAllMerchant.graphql";
+import BaseHero from "~/components/base/BaseHero"
 
 export default {
+  components: {
+    BaseHero
+  },
   async asyncData({ app }) {
     try {
       const response = await app.$axios.post(process.env.API_PATH, {
