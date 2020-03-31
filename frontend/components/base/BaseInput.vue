541eb4473888e4a8b37a0152e0e7ba3420f08147
ProsperChiduku <prosper@mindvalley.com>
2020-04-01 00:04:17 +0800

    added the frontend implementation of BaseInput

---
+++ frontend/components/base/BaseInput.vue
@@ -0,0 +1,44 @@
+<template>
+
+    <div
+      class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5"
+    >
+      <label
+        class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2"
+        :for="FormInputClassId"
+      >{{FormInputLabelText}}</label>
+      <div class="mt-1 sm:mt-0 sm:col-span-2">
+        <div class="max-w-xs rounded-md shadow-sm">
+          <input
+            class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+            :id="FormInputClassId"
+            v-model="firstName"
+            @input='$emit("input", firstName )'
+          />
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "BaseInput",
+        props:{
+            FormInputLabelText:{
+                type: String,
+                default: ""
+            },
+            FormInputClassId:{
+                type: String,
+                default: ""
+            },
+            data(){
+                firstName: ""
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/pages/merchant/create.vue
+++ frontend/pages/merchant/create.vue
@@ -141,22 +141,12 @@
                   >Use a permanent address where you can receive mail.</p>
                 </div>
                 <div class="mt-6 sm:mt-5">
-                  <div
-                    class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5"
-                  >
-                    <label
-                      class="block text-sm font-medium leading-5 text-gray-700 sm:mt-px sm:pt-2"
-                      for="first_name"
-                    >First name</label>
-                    <div class="mt-1 sm:mt-0 sm:col-span-2">
-                      <div class="max-w-xs rounded-md shadow-sm">
-                        <input
-                          class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
-                          id="first_name"
-                        />
-                      </div>
-                    </div>
-                  </div>
+                <BaseInput
+                  :FormInputLabelText="'Firstname'"
+                  :FormInputClassId="'first_name'"
+                  v-model="firstName"
+                />
+
 
                   <div
                     class="mt-6 sm:mt-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5"
@@ -451,19 +441,22 @@
     import ImageUploader from "~/components/ImageUploader";
     import InsertMerchant from "~/services/InsertMerchant";
     import BaseTextArea from "~/components/base/BaseTextArea";
+    import BaseInput from "~/components/base/BaseInput";
 
     export default {
         components: {
             // Map,
             ImageUploader,
-            BaseTextArea
+            BaseTextArea,
+            BaseInput
         },
         data() {
             return {
                 name: "",
                 location: "1,1",
                 images: [],
-                description: ""
+                description: "",
+                firstName:""
             };
         },
         computed: {
