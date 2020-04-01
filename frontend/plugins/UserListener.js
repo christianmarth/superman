1bc986107d4e13ed939046ff38f3f4cd7f7ce17a
aaron <aaron@mindvalley.com>
2020-04-01 19:30:17 +0800

    better user login/session management

--- frontend/plugins/UserListener.js
+++ frontend/plugins/UserListener.js
@@ -1,5 +1,6 @@
 // This implementation is recommended here:
 // https://firebase.google.com/docs/auth/web/manage-users?authuser=1
+import query from "~/services/InsertUser/InsertUser.graphql";
 
 export default ({ app }) => {
     app.$storage.syncUniversal("accessToken")
@@ -8,22 +9,31 @@ export default ({ app }) => {
 
     app.$fireAuth.onAuthStateChanged(user => {
         if (user) {
-            user = JSON.parse(JSON.stringify(user))
-            app.$storage.setState("user", user)
-            app.$storage.setLocalStorage("user", user)
-            app.$storage.setState("loggedIn", true)
-            app.$storage.setLocalStorage("loggedIn", true)
+            user.getIdToken(true).then(token => {
+                const parsedUser = JSON.parse(JSON.stringify(user))
+                app.$storage.setState("user", parsedUser)
+                app.$storage.setState("loggedIn", true)
 
-            // Store accessToken seperately for convenience
-            // Also synchronises it to Vuex/Local Storage/Cookies
-            app.$storage.setUniversal(
-                "accessToken",
-                user.stsTokenManager.accessToken
-            );
+                // Store accessToken seperately for convenience
+                // Also synchronises it to Vuex/Local Storage/Cookies
+                app.$storage.setUniversal(
+                    "accessToken",
+                    token
+                );
+                app.$axios.setToken(token, 'Bearer')
+                app.$axios.post(process.env.API_PATH, {
+                    query: query,
+                    variables: {
+                        uid: user.uid,
+                        photo_url: user.photoURL,
+                        name: user.displayName,
+                        email: user.email
+                    }
+                })
+            })
         } else {
             // No user is signed in.
             app.$storage.setState("loggedIn", false)
-            app.$storage.setLocalStorage("loggedIn", false)
         }
     });
 
